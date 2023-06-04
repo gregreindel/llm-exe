d3eca440406d9dbd5aa9ab9112bb1f505ac39e6a
Greg Reindel <greg.reindel@gmail.com>
2023-06-04 10:57:15 -0500

    ci, add test

---
+++ __tests__/unit/core-export.ts
@@ -0,0 +1,56 @@
+import { llmExe } from "@/index";
+
+describe("llmExe", () => {
+  test("has all exports", () => {
+    /**
+     * Utilities
+     */
+    expect(llmExe).toHaveProperty("utils");
+    /**
+     * Llm
+     */
+    expect(llmExe).toHaveProperty("BaseLlm");
+    expect(llmExe).toHaveProperty("OpenAI");
+    expect(llmExe).toHaveProperty("createLlmOpenAi");
+    /**
+     * Prompt
+     */
+    expect(llmExe).toHaveProperty("BasePrompt");
+    expect(llmExe).toHaveProperty("TextPrompt");
+    expect(llmExe).toHaveProperty("ChatPrompt");
+    expect(llmExe).toHaveProperty("createPrompt");
+    expect(llmExe).toHaveProperty("createChatPrompt");
+    /**
+     * Parsers
+     */
+    expect(llmExe).toHaveProperty("BaseParser");
+    expect(llmExe).toHaveProperty("CustomParser");
+    expect(llmExe).toHaveProperty("createParser");
+    expect(llmExe).toHaveProperty("createCustomParser");
+    /**
+     * Core Functions
+     */
+    expect(llmExe).toHaveProperty("BaseExecutor");
+    expect(llmExe).toHaveProperty("createCoreExecutor");
+    expect(llmExe).toHaveProperty("createLlmExecutor");
+
+    /**
+     * Callable
+     */
+    expect(llmExe).toHaveProperty("useExecutors");
+    expect(llmExe).toHaveProperty("createCallableExecutor");
+    /**
+     * Embedding
+     */
+    expect(llmExe).toHaveProperty("EmbeddingOpenAI");
+    expect(llmExe).toHaveProperty("PineconeVectorStore");
+    /**
+     * State
+     */
+    expect(llmExe).toHaveProperty("DefaultState");
+    expect(llmExe).toHaveProperty("BaseStateItem");
+    expect(llmExe).toHaveProperty("DefaultStateItem");
+    expect(llmExe).toHaveProperty("createState");
+    expect(llmExe).toHaveProperty("createStateItem");
+  });
+});
