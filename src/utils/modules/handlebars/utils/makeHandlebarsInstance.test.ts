8dab48dc2216cc59dfa127d1aadd28371a7c6bfe
Greg Reindel <greg.reindel@gmail.com>
2025-05-11 14:43:02 -0500

    remove ability to include helpers and partials from a path.

--- src/utils/modules/handlebars/utils/makeHandlebarsInstance.test.ts
+++ src/utils/modules/handlebars/utils/makeHandlebarsInstance.test.ts
@@ -13,75 +13,50 @@ jest.mock("@/utils/modules/handlebars/templates");
 jest.mock("@/utils/modules/handlebars/helpers");
 
 describe("makeHandlebarsInstance", () => {
-    let handlebarsInstance = makeHandlebarsInstance(Handlebars);
-    // ReturnType<typeof makeHandlebarsInstance>;
+  let handlebarsInstance = makeHandlebarsInstance(Handlebars);
+  // ReturnType<typeof makeHandlebarsInstance>;
 
-    beforeEach(() => {
-        (getEnvironmentVariable as jest.Mock).mockReturnValue(undefined);
-        // handlebarsInstance = makeHandlebarsInstance(Handlebars);
-    });
+  beforeEach(() => {
+    (getEnvironmentVariable as jest.Mock).mockReturnValue(undefined);
+    // handlebarsInstance = makeHandlebarsInstance(Handlebars);
+  });
 
-    it("should create a new Handlebars instance", () => {
-        expect(handlebarsInstance).toBeDefined();
-    });
+  it("should create a new Handlebars instance", () => {
+    expect(handlebarsInstance).toBeDefined();
+  });
 
-    it("should register 'with' helper", () => {
-        expect(handlebarsInstance.helpers.with).toBeDefined();
-    });
+  it("should register 'with' helper", () => {
+    expect(handlebarsInstance.helpers.with).toBeDefined();
+  });
 
-    it("should register 'cut' helper", () => {
-        expect(handlebarsInstance.helpers.cut).toBeDefined();
-    });
+  it("should register 'cut' helper", () => {
+    expect(handlebarsInstance.helpers.cut).toBeDefined();
+  });
 
-    it("should register 'substring' helper", () => {
-        expect(handlebarsInstance.helpers.substring).toBeDefined();
-    });
+  it("should register 'substring' helper", () => {
+    expect(handlebarsInstance.helpers.substring).toBeDefined();
+  });
 
-    it("should register 'unless' helper", () => {
-        expect(handlebarsInstance.helpers.unless).toBeDefined();
-    });
+  it("should register 'unless' helper", () => {
+    expect(handlebarsInstance.helpers.unless).toBeDefined();
+  });
 
-    it("should register 'hbsInTemplate' helper", () => {
-        expect(handlebarsInstance.helpers.hbsInTemplate).toBeDefined();
-    });
+  it("should register 'hbsInTemplate' helper", () => {
+    expect(handlebarsInstance.helpers.hbsInTemplate).toBeDefined();
+  });
 
+  it("should register 'hbsInTemplate' helper", () => {
+    expect(
+      handlebarsInstance.helpers.hbsInTemplate.call(null, "Hello {{world}}!", {
+        world: "World",
+      })
+    ).toBeDefined();
+  });
 
-    it("should register 'hbsInTemplate' helper", () => {
-        expect(handlebarsInstance.helpers.hbsInTemplate.call(null, "Hello {{world}}!", {world: "World"})).toBeDefined();
-    });
+  it("should register context partials", () => {
+    (contextPartials as any).partials = { partial1: "<div>Partial 1</div>" };
+    handlebarsInstance = makeHandlebarsInstance(Handlebars);
 
-
-    it("should register custom helpers if CUSTOM_PROMPT_TEMPLATE_HELPERS_PATH is set", () => {
-        const mockHelpers = { customHelper: jest.fn() };
-        (getEnvironmentVariable as jest.Mock).mockReturnValue("/path/to/helpers");
-        jest.mock("/path/to/helpers", () => mockHelpers, { virtual: true });
-
-        handlebarsInstance = makeHandlebarsInstance(Handlebars);
-        expect(handlebarsInstance.helpers.customHelper).toBeDefined();
-
-        // expect(registerHelpers).toHaveBeenCalledWith(
-        //     expect.arrayContaining([{ handler: mockHelpers.customHelper, name: "customHelper" }]),
-        //     handlebarsInstance
-        // );
-    });
-
-    it("should register context partials", () => {
-        (contextPartials as any).partials = { partial1: "<div>Partial 1</div>" };
-        handlebarsInstance = makeHandlebarsInstance(Handlebars);
-
-        expect(handlebarsInstance.partials.partial1).toBeDefined();
-    });
-
-    it("should register custom partials if CUSTOM_PROMPT_TEMPLATE_PARTIALS_PATH is set", () => {
-        const mockPartials = { customPartial: "<div>Custom Partial</div>" };
-        (getEnvironmentVariable as jest.Mock).mockReturnValue("/path/to/partials");
-        jest.mock("/path/to/partials", () => mockPartials, { virtual: true });
-
-        handlebarsInstance = makeHandlebarsInstance(Handlebars);
-
-        expect(_registerPartials).toHaveBeenCalledWith(
-            expect.arrayContaining([{ name: "customPartial", template: mockPartials.customPartial }]),
-            handlebarsInstance
-        );
-    });
+    expect(handlebarsInstance.partials.partial1).toBeDefined();
+  });
 });
